73b548683b557c3333d2b94990677ffa48b28025
Mariana <mariana.oliveira@uniriotec.br>
2018-06-14 22:29:36 -0300

    [cdu01] Adicao da tela Aplicacao (Tudo funcionando)

---
+++ SistemaBancario/Views/InicialAdministradorAplicacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SistemaBancario.Views
+{
+    public partial class InicialAdministradorAplicacao : SistemaBancario.Views.TemplateInicialAdministrador
+    {
+        public InicialAdministradorAplicacao()
+        {
+            InitializeComponent();
+        }
+    }
+}
